e238a50fbe6923ed4accaa5f92f436e3e2cbddcc
Harsha-web-coder <77110520+Harsha-web-coder@users.noreply.github.com>
2023-03-27 22:52:00 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+adj1 = input("Adjective: ")
+verb1 = input("Verb: ")
+adj2 = input("Adjective: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj1}! It makes me so excited all the time because \
+I love to {verb1}. Stay healthy and {adj2} like I am a {famous_person}!"
+
+print(madlib)
+"""
+OUTPUT:
+Adjective: amazing
+Verb: coding
+Adjective: active
+Famous person: coder
+Computer programming is so amazing! It makes me so excited all the time because I love to coding. Stay healthy and active like I am a coder!
+"""
